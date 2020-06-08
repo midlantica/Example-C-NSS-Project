0260aae91d4deb4d60cbf20934c0741c3cbef1f9
drewharper <druharper@gmail.com>
2020-06-08 13:07:08 -0500

    trying git in powershwll from VS

---
+++ Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Hello World! {i}");
+                //Console.WriteLine($"Yeahhhhhh!!");
+            }
+        }
+    }
+}
